679fe14ce76f05a9dcf33336e65b6165eb1f608f
Tangb <tanguy.bonneau@gmail.com>
2020-10-16 09:19:43 +0200

    Fix doc

--- docs/conf.py
+++ docs/conf.py
@@ -16,14 +16,34 @@ extensions = [
 source_suffix = '.rst'
 master_doc = 'index'
 language = None
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    u'_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '**/*.pyc',
+    'cleep/tests/**',
+    'cleep/modules/**',
+]
+add_module_names = False
 pygments_style = None
-html_theme = 'sphinx_rtd_theme'
+html_static_path = [
+    '_static'
+]
+html_css_files = [
+    '_static/cleep.css'
+]
 todo_include_todos = True
-autodoc_default_options = { 
-    'undoc-members': True,
-    'private-members': False,
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'prev_next_buttons_location': None,
+    'style_external_links': True,
+    'style_nav_header_background': '#607d8b',
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 6,
+    'includehidden': False,
 }
+
 def setup(app):
     app.add_css_file('cleep.css')
-    
+
